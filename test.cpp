97d26291317db919945d2be0f960376ca8bed719
Thales Francisco <thales@alvesnet.net>
2016-02-08 11:24:58 -0300

    adicionando caso teste

---
+++ test.cpp
@@ -0,0 +1,21 @@
+//Texto de exemplo código c-
+
+int funcao(string texto){
+	int a = 1;
+	int b = 2;
+	return a+b;
+}
+/*comentario
+de
+bloco*/
+
+int main(){
+	string texto = "texto";
+	funcao(texto);
+	if(a>b){
+		a==b;
+	} else {
+		a*b;
+	}
+	/* comentario de bloco /*aninhado /*do capeta */*/*/
+}
