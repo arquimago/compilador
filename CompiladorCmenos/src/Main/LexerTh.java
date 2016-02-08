998d709fd386bc9ef15013fc900307566e1a8943
Thales Francisco <thales@alvesnet.net>
2016-02-08 15:32:15 -0300

    Não tinha ido o LexerTh.java no outro commit

--- CompiladorCmenos/src/Main/LexerTh.java
+++ CompiladorCmenos/src/Main/LexerTh.java
@@ -10,49 +10,48 @@ import Cmenos.node.*;
 public class LexerTh extends Lexer {
 	
 	private int contador = 0;
-	private TComentarioCorpo comentario;
-	private StringBuffer texto;
+	private TComentarioE comentario = null;
+	private String texto = " ";
 	
 	public LexerTh(final PushbackReader in) {
 		super(in);
 		
 	}
 	
+	private void erroComentario() throws LexerException{
+		InvalidToken invalido = new InvalidToken("Token Invalido", token.getLine(), token.getPos());
+		throw new LexerException(invalido, "Token Invalido (linha: " + invalido.getLine() + " coluna: " + invalido.getPos() +"):" + texto );
+	}
+	
 	@Override
 	protected void filter() throws LexerException, IOException {
-		//Tokens new4 TComentarioCorpo (comentario + texto) e new5 TComentarioD (fecha)
-		/*Adicionar SEMPRE no Lexer.java as linhas:
-		if(state.id==0) this.state = State.COMENTARIO;
-		no método getToken() nos tokens TComentarioCorpo e TComentarioD
-		public final static State COMENTARIO = new State(1);
-		na classe interna State	*/
+		texto.replaceAll(" ","");
 		if(state.equals(State.COMENTARIO)){
 			if(comentario == null){
-				comentario = (TComentarioCorpo) token;
+				if (token instanceof TComentarioD){
+					erroComentario();
+				}
+				comentario = (TComentarioE) token;
 				contador ++;
 				token = null;
 			} else {
-				System.out.println("entrou no else do lexer pra comentario");
-				texto.append(token.getText());
-				if(token instanceof TComentarioCorpo) {
+				texto += token.getText();
+				if(token instanceof TComentarioE) {
 					contador++;
 				} else if (token instanceof TComentarioD) {
 					contador--;
-				} else {
-					InvalidToken invalido = new InvalidToken("Token Invalido", comentario.getLine(), comentario.getPos());
-					throw new LexerException(invalido, "Token Invalido:" + texto); 
+				} else if (token instanceof EOF) {
+					erroComentario(); 
 				}
 				if(contador!=0){
 					token=null;
 				} else {
-					comentario.setText(texto.toString());
+					comentario.setText(texto);
 					token = comentario;
-					state = State.INITIAL;
+					state = State.NORMAL;
 					comentario = null;
 				}
 			}
 		}
     }
-	
-
 }
